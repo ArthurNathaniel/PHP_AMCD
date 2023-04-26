55627a95c6d96f26393ce0fc05b287ecd08dc320
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-04-26 12:50:58 -0500

    changes

--- corrugated_roofing.php
+++ corrugated_roofing.php
@@ -81,8 +81,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header">
                                 <button class="accordion-button collapsed" style="color:#000;" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    <i class="fas fa-toolbox"></i>
-                                    <span style="margin-left: 5%;">Lightweight</span>
+                                    <i class="fas fa-hard-hat"></i> <span style="margin-left: 5%;">Lightweight</span>
                                 </button>
                             </h2>
                             <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
@@ -108,7 +107,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" style="color:#000;" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                    <i class="fas fa-tools"></i> <span style="margin-left: 5%;"> Ease of installation</span>
+                                    <i class="fas fa-hard-hat"></i> <span style="margin-left: 5%;"> Ease of installation</span>
                                 </button>
                             </h2>
                             <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
