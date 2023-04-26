68421d2a7aa5418b131c39887409557caa3c8a77
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-04-26 11:13:03 -0500

    chnages

--- idt.php
+++ idt.php
@@ -63,7 +63,7 @@
                                 </a>
                             </div>
                             <div class="col-md-4 long-img">
-                                <a href="https://placehold.jp/800x500.png" data-lightbox="gallery" data-title="IDT" download>
+                                <a href="https://placehold.jp/800x500.png" data-lightbox="gallery" data-title="IDT 4" download>
                                     <img src="https://placehold.jp/250x250.png" alt="idt" width="100%">
                                 </a>
                             </div>
