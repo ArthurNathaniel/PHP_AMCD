c2dff641c7188c71c938f7e748e51e398aa2fecb
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-04-26 11:10:55 -0500

    changes

--- ibr.php
+++ ibr.php
@@ -53,7 +53,7 @@
                         </div>
                         <div class="row long-three">
                             <div class="col-md-4 long-img">
-                                <a href="https://placehold.jp/800x500.png" data-lightbox="gallery" data-title="IBR" download>
+                                <a href="https://placehold.jp/800x500.png" data-lightbox="gallery" data-title="IBR 2" download>
                                     <img src="https://placehold.jp/250x250.png" alt="ibr" width="100%">
                                 </a>
                             </div>
