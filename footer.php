376bc61897e082c4dd72e97152395b69a054ef72
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-04-26 10:49:12 -0500

    changes

--- footer.php
+++ footer.php
@@ -1,10 +1,47 @@
 <footer>
     <div class="row gy-5">
         <div class="col-md-3">
-            <img src="images/footer.png" alt="" style="width: 80%;">
+            <img src="images/footer.png" alt="" style="width: 50%;">
+            <h5 style="margin-top:15px; color:#fff; font-weight:900; text-shadow: 0px 2px 9px #0101017d;">FOLLOW US ON:</h5>
+
+            <div class="media">
+                <div class="m-media">
+                    <li><a href="https://web.facebook.com/people/Agyeiwaa-Memorial-Company-AMCO/100069205541683/?_rdc=1&_rdr" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                </div>
+                <div class="m-media">
+                    <li><a href="" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                </div>
+                <div class="m-media">
+                    <li><a href="https://twitter.com/AmcoghanaLtd" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                </div>
+                <div class="m-media">
+                    <li><a href="https://www.linkedin.com/company/agyeiwaa-memorial-company-limited/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                </div>
+            </div>
 
         </div>
+        <style>
+            .media {
+                display: flex;
 
+            }
+
+            .m-media {
+                width: 15%;
+            }
+
+            .m-media a {
+                /* font-size: 30px; */
+                text-shadow: 0px 2px 9px #0101017d;
+            
+
+            }
+
+            .m-media a:hover {
+                color: antiquewhite;
+
+            }
+        </style>
         <div class="col-md-3">
             <h3>QUICK LINES</h3>
             <li><a href="index.php">Home</a></li>
@@ -26,7 +63,7 @@
             <li><a href="https://goo.gl/maps/boxVTAfqB1mNFXEJ6"> <i class="fas fa-map-marker-alt"></i>
                     Adako Jachie Junction, </a></li>
             <li><a href="https://goo.gl/maps/boxVTAfqB1mNFXEJ6"> <i class="fas fa-map-marker-alt"></i>
-                    Opposite Kwamo Kumasi, Ghana</a></li>
+                    Opposite Kwamo Kumasi</a></li>
             <li><a href="tel:+233556120878"> <i class="fas fa-phone-alt"></i> +233 55 612 0878</a></li>
             <li><a href="tel:+233559558041"> <i class="fas fa-phone-alt"></i> +233 55 955 8041</a></li>
         </div>
